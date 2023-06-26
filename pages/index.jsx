c0016af4051d1cb4ef11eace2a44c914b5d1deda
hiep pham <hiep.pham@tomosia.com>
2023-06-27 01:47:50 +0900

    add

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,8 @@
+import { Image } from "antd"
 import React from "react"
 export default function Home() {
-  return <div>ok</div>
+  return <div className="flex flex-col items-center">
+    <h1 className="text-[30px] font-bold">WellCome ADMIN</h1>
+    <Image preview={false} src="https://tripbike.net/wp-content/uploads/2021/06/thue-xe-may-da-nang-tripbike.jpg" alt=""/>
+  </div>
 }
